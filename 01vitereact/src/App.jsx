2473ab9bbd800389610c13cebb199e7fc13097fe
Prasun Mittal <prasunmittal09@gmail.com>
2026-05-17 17:28:03 +0530

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,11 @@
 import Chai from "./chai"
 
 function App() {
+  const username = "Prasun"
   return (
     <>
       <Chai />
+      <p> userName is <b>{username}</b></p>
       <p>test</p>
     </>
   )
